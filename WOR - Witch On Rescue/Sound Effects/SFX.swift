ee63bc09094e31b0a80663638d01b93ac09cdcd5
DaneOnishi <daniellaonishi07@gmail.com>
2022-02-08 23:13:21 -0300

    Adds structure for music player

---
+++ WOR - Witch On Rescue/Sound Effects/SFX.swift
@@ -0,0 +1,73 @@
+//
+//  SFX.swift
+//  WOR - Witch On Rescue
+//
+//  Created by Daniella Onishi on 08/02/22.
+//
+
+import Foundation
+import AVFoundation
+
+class SFXMusicSingleton: NSObject, AVAudioPlayerDelegate {
+    static let shared = SFXMusicSingleton()
+    fileprivate var currentPlayer: AVAudioPlayer?
+    private override init() {}
+    
+    var players: [URL: AVAudioPlayer] = [:]
+    var duplicatePlayers: [AVAudioPlayer] = []
+    
+    func playSound(soundFileName: String) {
+        
+        guard let bundle = Bundle.main.path(forResource: soundFileName, ofType: "mp3")
+        else{ return }
+        
+        let soundFileNameURL = URL(fileURLWithPath: bundle)
+        if let player = players[soundFileNameURL] {
+            
+            if !player.isPlaying {
+                player.prepareToPlay()
+                player.play()
+            } else {
+                
+                do {
+                    let duplicatePlayer = try AVAudioPlayer(contentsOf: soundFileNameURL)
+                    
+                    duplicatePlayer.delegate = self
+                    
+                    duplicatePlayers.append(duplicatePlayer)
+                    duplicatePlayer.prepareToPlay()
+                    duplicatePlayer.play()
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            }
+        } else {
+            do {
+                let player = try AVAudioPlayer(contentsOf: soundFileNameURL)
+                players[soundFileNameURL] = player
+                player.prepareToPlay()
+                player.play()
+            } catch let error {
+                print(error.localizedDescription)
+            }
+        }
+        currentPlayer?.numberOfLoops = 100
+    }
+    
+    
+    func playMainMusic() {
+        playSound(soundFileName: "")
+    }
+    
+    func soundPopPops() {
+        playSound(soundFileName: "")
+    }
+}
+
+        
+    
+
+
+
+
+
